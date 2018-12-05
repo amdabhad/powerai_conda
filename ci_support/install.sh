1ecdcb3816c9d596f3e68cbc7c0668a77306e8fd
Amol <amdabhad@in.ibm.com>
2018-12-05 13:55:55 +0530

    Update install.sh

--- ci_support/install.sh
+++ ci_support/install.sh
@@ -1,14 +1,17 @@
 # Install miniconda
 sudo apt-get update
 
-ARCH=`uname -m`
-echo "AMOL: ARCH: $ARCH"
+arch=`uname -m`
+recipe_dir=$1
+
+echo "AMOL: arch: $arch"
 echo "AMOL: OS:"
 cat /etc/os-release
+
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-${ARCH}.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-${arch}.sh -O miniconda.sh;
 else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-${ARCH}.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-${arch}.sh -O miniconda.sh;
 fi
 chmod +x miniconda.sh 
 bash miniconda.sh -b -p $HOME/miniconda
@@ -24,5 +27,5 @@ conda install conda-build
 sudo apt-get install lftp
 
 # Build conda package
-cd conda-recipes/$1
+cd conda-recipes/${recipe_dir}
 conda build .
