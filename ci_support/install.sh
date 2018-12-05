f6a3801af56b97d5ca2749f5a219458a3ac805aa
Amol <amdabhad@in.ibm.com>
2018-12-05 11:30:11 +0530

    Update install.sh

--- ci_support/install.sh
+++ ci_support/install.sh
@@ -3,13 +3,12 @@ if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
 	wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-ppc64le.sh -O miniconda.sh;
 else
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-ppc64le.sh -O miniconda.shfi
-
+fi
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 echo 'PATH =' $PATH
 conda config --set always_yes yes
 conda info -a
-
 
 # Install conda build
 conda install conda-build
