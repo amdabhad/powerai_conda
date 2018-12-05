f523602096552a0affa8b73360f76728edd42f0f
Amol <amdabhad@in.ibm.com>
2018-12-05 11:58:17 +0530

    Update install.sh

--- ci_support/install.sh
+++ ci_support/install.sh
@@ -1,8 +1,11 @@
 # Install miniconda
+
+ARCH=`uname -m`
+echo "ARCH: $ARCH"
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-	wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-ppc64le.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-${ARCH}.sh -O miniconda.sh;
 else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-ppc64le.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-${ARCH}.sh -O miniconda.sh;
 fi
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
