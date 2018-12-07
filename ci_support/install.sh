06c1407540d76114591ffce21d97f8378aa30255
Amol <amdabhad@in.ibm.com>
2018-12-07 11:47:34 +0530

    openjdk version rollback

--- ci_support/install.sh
+++ ci_support/install.sh
@@ -2,17 +2,13 @@
 sudo apt-get update
 
 ARCH=`uname -m`
-echo 
+echo "ARCH: $ARCH"
 RECIPE_DIR=$1
 
-echo "ARCH: $ARCH"
-echo "OS:"
-cat /etc/os-release
-
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-${ARCH}.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-ppc64le.sh -O miniconda.sh;
 else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-${ARCH}.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-ppc64le.sh -O miniconda.sh;
 fi
 chmod +x miniconda.sh 
 bash miniconda.sh -b -p $HOME/miniconda
