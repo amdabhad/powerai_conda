0a55755d950ceb10cfce7f5807acdcf075b9a54e
Amol <amdabhad@in.ibm.com>
2018-12-07 10:34:55 +0530

    zip test

--- ci_support/install.sh
+++ ci_support/install.sh
@@ -1,17 +1,18 @@
 # Install miniconda
 sudo apt-get update
 
-arch=`uname -m`
-recipe_dir=$1
+ARCH=`uname -m`
+echo 
+RECIPE_DIR=$1
 
-echo "AMOL: arch: $arch"
-echo "AMOL: OS:"
+echo "ARCH: $ARCH"
+echo "OS:"
 cat /etc/os-release
 
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-${arch}.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-${ARCH}.sh -O miniconda.sh;
 else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-${arch}.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-${ARCH}.sh -O miniconda.sh;
 fi
 chmod +x miniconda.sh 
 bash miniconda.sh -b -p $HOME/miniconda
@@ -27,5 +28,5 @@ conda install conda-build
 sudo apt-get install lftp
 
 # Build conda package
-cd conda-recipes/${recipe_dir}
+cd conda-recipes/${RECIPE_DIR}
 conda build .
